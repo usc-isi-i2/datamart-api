b4f18b79deb1284a8c736de59b900695b3fa45d8
Itay Zandbank <itay@chelem.co.il>
2020-06-10 22:00:15 +0300

    Add a method that retrieves country qnodes

--- api/variable/get.py
+++ api/variable/get.py
@@ -86,23 +86,8 @@ class VariableGetter:
             Location.initialize(current_app.config)
             location = Location()
 
-    def get(self, dataset=None, variable=None):
+    def get(self, dataset, variable):
         self.init_resources()
-        if dataset == 'Qwikidata':
-            dataset = 'Wikidata'
-        if dataset not in ['Wikidata', 'UAZ']:
-            content = {
-                'Error': f'path not found: /datasets/{dataset}',
-                'Usage': 'Use path /datasets/Wikidata/variables/{variable}'
-            }
-            return content, 404
-        if variable is None:
-            content = {
-                'Error': f'path not found: /datasets/{dataset}/variables/{variable}',
-                'Usage': f'Use path /datasets/{dataset}/variables/{{PNode}}',
-                'Example': f'Use path /datasets/{dataset}/variables/VP1200149'
-            }
-            return content, 404
 
         include_cols = []
         exclude_cols = []
@@ -170,6 +155,7 @@ class VariableGetter:
 
     def get_direct(self, dataset, variable, include_cols, exclude_cols, limit, main_subjects=[]):
         provider = SQLProvider()
+
         result = provider.query_variable(dataset, variable)
         if not result:
             content = {
@@ -340,3 +326,33 @@ class SQLProvider:
         print(query)
 
         return query_to_dicts(query)
+
+    def query_country_qnodes(self, countries):
+        # Translates countries to Q-nodes. Returns a dictionary of each input country and its QNode (None if not found)
+        # We look for countries in a case-insensitive fashion.
+        if not countries:
+            return {}
+
+        lower_countries = [country.lower() for country in countries]
+        quoted_countries = [f"'{country}'" for country in lower_countries]
+        countries_in = ', '.join(quoted_countries)
+
+        query = f'''
+            SELECT e_country.node1 as qnode, s_country_label.text AS country
+            	FROM edges e_country
+	            JOIN edges e_country_label ON (e_country_label.node1=e_country.node1 AND e_country_label.label='label')
+	            JOIN strings s_country_label ON (e_country_label.id=s_country_label.edge_id)
+            WHERE e_country.label='P31' AND e_country.node2='Q6256' AND LOWER(s_country_label.text) IN ({countries_in})
+        ''';
+        rows = query_to_dicts(query)
+
+        result_dict = { row['country']: row['qnode'] for row in rows }
+
+        # The result dictionary contains all the countries we have found, we need to add those we did not find
+        found_countries = set([country.lower() for country in result_dict.keys()])
+        for country in countries:
+            if country.lower() not in found_countries:
+                result_dict[country] = None
+
+        return result_dict
+
