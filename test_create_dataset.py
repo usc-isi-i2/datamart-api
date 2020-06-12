514a8c770d166b057bcfea3efe962bd2619c46e2
Ke-Thia Yao <kethia.yao@gmail.com>
2020-06-11 20:25:18 -0700

    update name

--- test_create_dataset.py
+++ test_create_dataset.py
@@ -1,13 +1,13 @@
 from requests import post
 wdi = {
     "name": "World Development Indicators",
-    "shortName": "WDI2",
+    "short_name": "WDI2",
     "description": "Indicators from World Bank",
     "url": "https://data.worldbank.org/indicator"
 }
 gdp_ppp = {
     "name": "gross domestic product based on purchasing power parity",
-    "shortName": "GDP2"
+    "short_name": "GDP2"
 }
 wdi_response = post('http://127.0.0.1:5000/metadata/datasets', json=wdi)
 print(wdi_response.text)
