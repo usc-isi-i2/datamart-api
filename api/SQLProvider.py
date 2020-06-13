77e028c6baa5c500d8c0be9dd59f87fd9a12f3ff
Itay Zandbank <itay@chelem.co.il>
2020-06-13 21:55:56 +0300

    Use Postgres's full text search capabilities

--- api/SQLProvider.py
+++ api/SQLProvider.py
@@ -436,54 +436,32 @@ class SQLProvider:
             sql = "LEFT " + sql;
         return '\t' + sql  + '\n';
 
-    def fuzzy_query_variables(self, terms):
-        if not terms:
+    def fuzzy_query_variables(self, question):
+        if not question:
             return []
 
-        # We need to use to SELECTs, as explained here: https://stackoverflow.com/a/12866110/871910
-        # fuzzied_terms = [f"(variable_text ILIKE '%{term}%')" for term in terms]
-        # fuzzied_clause = " OR".join(fuzzied_terms)
+        # Use Postgres's full text search capabilities
+        sql = f"""
+        SELECT fuzzy.variable_id, fuzzy.dataset_qnode, fuzzy.name,  ts_rank(variable_text, plainto_tsquery('{question}')) AS rank FROM
+            (SELECT e_var_name.node2 AS variable_id,
+                    -- e_dataset_name.node2 AS dataset_id,
+                    e_dataset.node1 AS dataset_qnode,
+                    to_tsvector(CONCAT(s_description.text, ' ', s_name.text, ' ', s_label.text)) AS variable_text,
+                    CONCAT(s_name.text, ' ', s_label.text) as name
+                FROM edges e_var
+                JOIN edges e_var_name ON (e_var_name.node1=e_var.node1 AND e_var_name.label='P1813')
+                JOIN edges e_dataset ON (e_dataset.label='P2006020003' AND e_dataset.node2=e_var.node1)
+                        -- JOIN edges e_dataset_name ON (e_dataset_name.node1=e_dataset.node1 AND e_dataset_name.label='P1813')
+                LEFT JOIN edges e_description JOIN strings s_description ON (e_description.id=s_description.edge_id) ON (e_var.node1=e_description.node1 AND e_description.label='description')
+                LEFT JOIN edges e_name JOIN strings s_name ON (e_name.id=s_name.edge_id) ON (e_var.node1=e_name.node1 AND e_name.label='P1813')
+                LEFT JOIN edges e_label JOIN strings s_label ON (e_label.id=s_label.edge_id) ON (e_var.node1=e_label.node1 AND e_label.label='label')
 
-        fuzzied_terms = [f"(variable_text ILIKE '%{term}%')" for term in terms]
-        fuzzied_clause = " OR".join(fuzzied_terms)
-
-        # query = f"""
-        # SELECT fuzzy.* FROM (
-        # SELECT e_var.node1 AS variable_id,
-	# 		   e_var_name.node2 AS variable_short_name,
-	# 		   e_dataset.node1 AS dataset_id,
-        #        -- e_dataset_name.node2 AS dataset_short_name,
-        #        CONCAT(s_description.text, ' ', s_name.text, ' ', s_label.text) AS variable_text
-        # FROM edges e_var
-	# 	JOIN edges e_var_name ON (e_var_name.node1=e_var.node1 AND e_var_name.label='P1813')
-        # JOIN edges e_dataset ON (e_dataset.label='P2006020003' AND e_dataset.node2=e_var.node1)
-	# 	-- JOIN edges e_dataset_name ON (e_dataset_name.node1=e_dataset.node1 AND e_dataset_name.label='P1813')
-        # LEFT JOIN edges e_description JOIN strings s_description ON (e_description.id=s_description.edge_id) ON (e_var.node1=e_description.node1 AND e_description.label='description')
-        # LEFT JOIN edges e_name JOIN strings s_name ON (e_name.id=s_name.edge_id) ON (e_var.node1=e_name.node1 AND e_name.label='P1813')
-	# 	LEFT JOIN edges e_label JOIN strings s_label ON (e_label.id=s_label.edge_id) ON (e_var.node1=e_label.node1 AND e_label.label='label')
-
-        # WHERE e_var.label='P31' AND e_var.node2='Q50701') AS fuzzy
-        # WHERE {fuzzied_clause}
-        # """
-        query = f"""
-        SELECT fuzzy.* FROM (
-        SELECT e_var_name.node2 AS variable_id,
-               -- e_dataset_name.node2 AS dataset_id,
-               e_dataset.node1 AS dataset_qnode,
-               CONCAT(s_description.text, ' ', s_name.text, ' ', s_label.text) AS variable_text,
-               CONCAT(s_name.text, ' ', s_label.text) as name
-        FROM edges e_var
-		JOIN edges e_var_name ON (e_var_name.node1=e_var.node1 AND e_var_name.label='P1813')
-        JOIN edges e_dataset ON (e_dataset.label='P2006020003' AND e_dataset.node2=e_var.node1)
-                -- JOIN edges e_dataset_name ON (e_dataset_name.node1=e_dataset.node1 AND e_dataset_name.label='P1813')
-        LEFT JOIN edges e_description JOIN strings s_description ON (e_description.id=s_description.edge_id) ON (e_var.node1=e_description.node1 AND e_description.label='description')
-        LEFT JOIN edges e_name JOIN strings s_name ON (e_name.id=s_name.edge_id) ON (e_var.node1=e_name.node1 AND e_name.label='P1813')
-		LEFT JOIN edges e_label JOIN strings s_label ON (e_label.id=s_label.edge_id) ON (e_var.node1=e_label.node1 AND e_label.label='label')
-
-        WHERE e_var.label='P31' AND e_var.node2='Q50701') AS fuzzy
-        WHERE {fuzzied_clause}
+            WHERE e_var.label='P31' AND e_var.node2='Q50701') AS fuzzy
+        WHERE variable_text @@ plainto_tsquery('{question}')
+        ORDER BY rank DESC
+        LIMIT 10
         """
-        print(query)
-        results = query_to_dicts(query)
+        print(sql)
+        results = query_to_dicts(sql)
 
         return results
--- api/metadata/main.py
+++ api/metadata/main.py
@@ -161,9 +161,8 @@ class FuzzySearchResource(Resource):
             return { 'Error': 'A variable query must be provided: keyword' }, 400
         provider = SQLProvider()
 
-        # This is a first version, the uses Postgre's ILIKE operator
-        terms, _ = split_objects_and_country(keyword)
-        results = provider.fuzzy_query_variables(terms)
+        # We're using Postgres's full text search capabilities for now
+        results = provider.fuzzy_query_variables(keyword)
 
         # Due to performance issues we will solve later, adding a JOIN to get the dataset short name makes the query
         # very inefficient, so results only have dataset_ids. We will now add the short_names
@@ -172,6 +171,5 @@ class FuzzySearchResource(Resource):
         for row in results:
             row['dataset_id'] = datasets[row['dataset_qnode']]
             del row['dataset_qnode']
-            del row['variable_text']
 
         return results
