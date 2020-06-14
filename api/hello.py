2189913de91103f7956e0f82e5e3bc7bb8b426c4
saggu <amandeep.s.saggu@gmail.com>
2020-06-14 13:14:30 -0700

    update the Hello message

--- api/hello.py
+++ api/hello.py
@@ -4,4 +4,5 @@ bp = Blueprint('hello', __name__)
 
 @bp.route('/')
 def hello() -> str:
-    return f"Backend is {current_app.config['STORAGE_BACKEND']}"
+    # return f"Backend is {current_app.config['STORAGE_BACKEND']}"
+    return f"Datamart Upload API"
