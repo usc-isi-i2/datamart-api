dd7d7016850be1efe2f96bf75c3fad1694f95eaa
saggu <amandeep.s.saggu@gmail.com>
2020-06-14 13:24:21 -0700

    link to documentation

--- api/hello.py
+++ api/hello.py
@@ -5,4 +5,4 @@ bp = Blueprint('hello', __name__)
 @bp.route('/')
 def hello() -> str:
     # return f"Backend is {current_app.config['STORAGE_BACKEND']}"
-    return f"Datamart Upload API"
+    return f"<html><a href='https://datamart-upload.readthedocs.io/en/latest/'>Datamart Upload API</a></html>"
