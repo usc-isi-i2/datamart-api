58ff24cce7b335f664aabd16221d93664cf1d41a
saggu <amandeep.s.saggu@gmail.com>
2020-06-22 13:31:04 -0700

    testing

--- test/test_get_dataset.py
+++ test/test_get_dataset.py
@@ -2,11 +2,12 @@ from requests import get
 import json
 
 # get all
-response = get('http://localhost:5000/metadata/datasets')
+# response = get('http://localhost:5000/metadata/datasets')
+response = get('http://dsbox02.isi.edu:14080/metadata/datasets')
 j = response.json()
-print(json.dumps(j))
+print(json.dumps(j, indent=2))
 print(response.status_code)
 
 # get one
-response = get('http://localhost:5000/metadata/datasets/OECD')
-print(response.json())
+# response = get('http://localhost:5000/metadata/datasets/OECD')
+# print(response.json())
