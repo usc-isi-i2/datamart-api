854d878780ddf6e61f03130605684228817f2b81
saggu <amandeep.s.saggu@gmail.com>
2020-07-08 14:07:08 -0700

    add code to get data for all variables, default 20

--- api/variable/get_all.py
+++ api/variable/get_all.py
@@ -1,12 +1,7 @@
-from flask import request, make_response, current_app
-
 import pandas as pd
-
-from db.sql import dal
-from db.sql.dal import Region
-from api.region_cache import region_cache
-from api.variable.get import UnknownSubjectError
+from flask import request, make_response
 from api.variable.get import VariableGetter
+from api.variable.get import UnknownSubjectError
 from api.metadata.main import VariableMetadataResource
 
 
@@ -15,9 +10,10 @@ class VariableGetterAll:
     vmr = VariableMetadataResource()
 
     def get(self, dataset):
-
+        request_variables = request.args.getlist('variable') or []
         include_cols = request.args.getlist('include') or []
         exclude_cols = request.args.getlist('exclude') or []
+
         limit = 20
         if request.args.get('limit') is not None:
             try:
@@ -29,8 +25,11 @@ class VariableGetterAll:
             regions = self.vg.get_query_region_ids()
         except UnknownSubjectError as ex:
             return ex.get_error_dict(), 404
-        variables = [x['variable_id'] for x in self.vmr.get(dataset)[0]][:limit]
-        print(variables)
+
+        if len(request_variables) > 0:
+            variables = request_variables
+        else:
+            variables = [x['variable_id'] for x in self.vmr.get(dataset)[0]][:limit]
 
         df_list = []
         for variable in variables:
