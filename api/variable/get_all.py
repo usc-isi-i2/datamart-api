942e34e98d8b89869e4de5ff7eff2c2648598697
saggu <amandeep.s.saggu@gmail.com>
2020-07-15 14:57:32 -0700

    change the download file format for all variables

--- api/variable/get_all.py
+++ api/variable/get_all.py
@@ -34,12 +34,24 @@ class VariableGetterAll:
         df_list = []
         for variable in variables:
             _ = self.vg.get_direct(dataset, variable, include_cols, exclude_cols, -1, regions, return_df=True)
+
             if _ is not None:
+                _ = self.reshape_canonical_data(_)
                 df_list.append(_)
         df = pd.concat(df_list)
-
+        df.drop(columns=['value', 'value_unit', 'variable_id', 'variable'], inplace=True)
         csv = df.to_csv(index=False)
         output = make_response(csv)
         output.headers['Content-Disposition'] = f'attachment; filename={dataset}_variables_all.csv'
         output.headers['Content-type'] = 'text/csv'
         return output
+
+    def reshape_canonical_data(self, df):
+        new_df = pd.DataFrame(columns=df.columns)
+        for i, row in df.iterrows():
+            row[f"{row['variable_id']}"] = row['value']
+            row['{}_unit'.format(row['variable_id'])] = row['value_unit']
+            row['{}_name'.format(row['variable_id'])] = row['variable']
+
+            new_df = new_df.append(row)
+        return new_df
