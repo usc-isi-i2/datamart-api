3abe2b3f7dbc422b2283f9586274e0c22e42cf08
saggu <amandeep.s.saggu@gmail.com>
2020-07-29 10:02:02 -0700

    debugiing statements

--- api/annotated/post.py
+++ api/annotated/post.py
@@ -21,7 +21,6 @@ class AnnotatedData(object):
         if not dataset_qnode:
             return {'Error': 'Dataset not found: {}'.format(dataset)}, 404
 
-        # df = pd.read_csv(request.files['file'], dtype=object, header=None).fillna('')
         df = pd.read_excel(request.files['file'], dtype=object, header=None).fillna('')
 
         validation_report, valid_annotated_file = self.va.validate(dataset, df=df)
@@ -33,16 +32,17 @@ class AnnotatedData(object):
         to_t2wml = ToT2WML(df, dataset_qnode=dataset_qnode)
         t2wml_yaml_dict = to_t2wml.get_dict()
         t2wml_yaml = to_t2wml.get_yaml()
-        # open('/tmp/t2.yaml', 'w').write(t2wml_yaml)
+        open('/tmp/t2.yaml', 'w').write(t2wml_yaml)
+
         # generate kgtk exploded file
         # TODO finish this section
         df = df.set_index(0)
-        gk = GenerateKgtk(df, t2wml_yaml_dict, dataset_qnode=dataset_qnode)
-
+        gk = GenerateKgtk(df, t2wml_yaml_dict, dataset_qnode=dataset_qnode, debug=True, debug_dir='/tmp')
+        gk.output_df_dict['wikifier.csv'].to_csv('/tmp/wikifier.csv', index=False)
         kgtk_exploded_df = gk.generate_edges_df()
 
-        # kgtk_exploded_df.to_csv('/tmp/t2wml-ann.csv', index=False)
-        gk.output_df_dict['wikifier.csv'].to_csv('/tmp/wikifier.csv', index=False)
+
+        kgtk_exploded_df.to_csv('/tmp/t2wml-ann.csv', index=False)
 
         # import to database
         import_kgtk_dataframe(kgtk_exploded_df, is_file_exploded=True)
