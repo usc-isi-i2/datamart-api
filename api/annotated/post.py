f651a8a7ec8bbb09d6f653bb5075c9ce39ab22ef
saggu <amandeep.s.saggu@gmail.com>
2020-08-07 16:26:35 -0700

    add option to not validate

--- api/annotated/post.py
+++ api/annotated/post.py
@@ -17,6 +17,8 @@ class AnnotatedData(object):
         self.vd = VariableDeleter()
 
     def process(self, dataset, is_request_put=False):
+        validate = request.args.get('validate', 'true').lower() == 'true'
+
         # check if the dataset exists
         dataset_qnode = dal.get_dataset_id(dataset)
 
@@ -33,9 +35,11 @@ class AnnotatedData(object):
         elif file_name.endswith('.csv'):
             df = pd.read_csv(request.files['file'], dtype=object, header=None).fillna('')
 
+
         validation_report, valid_annotated_file, rename_columns = self.va.validate(dataset, df=df)
-        if not valid_annotated_file:
-            return json.loads(validation_report), 400
+        if validate:
+            if not valid_annotated_file:
+                return json.loads(validation_report), 400
 
         for rn in rename_columns:
             df.iloc[rn[0], rn[1]] = rn[2]
@@ -66,7 +70,7 @@ class AnnotatedData(object):
                 print(self.vmr.delete(dataset, v))
 
         # import to database
-        import_kgtk_dataframe(kgtk_exploded_df, is_file_exploded=True)
+        # import_kgtk_dataframe(kgtk_exploded_df, is_file_exploded=True)
 
         variables_metadata = []
         for v in variable_ids:
