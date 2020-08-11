5ca5f36388c3bc55546891044d14e7fe6613b20a
Itay Zandbank <itay@chelem.co.il>
2020-08-11 12:50:52 +0300

    Disable put/post of canonical data

--- api/variable/main.py
+++ api/variable/main.py
@@ -10,13 +10,14 @@ class VariableResource(Resource):
         imp = VariableGetter()
         return imp.get(dataset, variable)
 
-    def put(self, dataset, variable):
-        put_data = CanonicalData()
-        return put_data.canonical_data(dataset, variable)
+    # Disable put and post, since we now support uploading annotated spreadsheets
+    # def put(self, dataset, variable):
+    #     put_data = CanonicalData()
+    #     return put_data.canonical_data(dataset, variable)
 
-    def post(self, dataset, variable):
-        put_data = CanonicalData()
-        return put_data.canonical_data(dataset, variable, is_request_put=False)
+    # def post(self, dataset, variable):
+    #     put_data = CanonicalData()
+    #     return put_data.canonical_data(dataset, variable, is_request_put=False)
 
     def delete(self, dataset, variable):
         imp = VariableDeleter()
