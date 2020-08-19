4be8cdd9b5c52713a195b91bfca57fa73ee7c371
saggu <amandeep.s.saggu@gmail.com>
2020-08-19 14:53:01 -0700

    accept multiple file, incomplete

--- api/t2wml/put.py
+++ api/t2wml/put.py
@@ -5,6 +5,9 @@ from db.sql.kgtk import import_kgtk_dataframe
 from api.variable.delete import VariableDeleter
 from api.metadata.main import VariableMetadataResource
 import csv
+import tempfile
+import subprocess
+import os
 
 
 class IngestT2WMLOutput(object):
@@ -20,12 +23,20 @@ class IngestT2WMLOutput(object):
         if not dataset_qnode:
             return {'Error': 'Dataset not found: {}'.format(dataset)}, 404
 
-        file_name = request.files['file'].filename
-        if not file_name.endswith('.tsv'):
+        t2wml_file_name = request.files['kgtk_output'].filename
+        item_defs_file_name = request.files['item_definitions'].filename
+        if not t2wml_file_name.endswith('.tsv'):
             return {"error": "Please upload a TSV file (T2WML output)"}, 400
 
-        df = pd.read_csv(request.files['file'], dtype=object, sep='\t', quoting=csv.QUOTE_NONE).fillna('')
+        if not item_defs_file_name.endswith('.tsv'):
+            return {"error": "Please upload a TSV file (T2WML output)"}, 400
 
+        t2wml_output_df = pd.read_csv(request.files['kgtk_output'], dtype=object, sep='\t',
+                                      quoting=csv.QUOTE_NONE).fillna('')
+        item_defs_df = pd.read_csv(request.files['item_definitions'], dtype=object, sep='\t',
+                                   quoting=csv.QUOTE_NONE).fillna('')
+
+        df = self.convert_t2wml_files(t2wml_output_df, item_defs_df)
         variable_ids = self.identify_variables(df)
 
         if is_request_put:
@@ -58,3 +69,30 @@ class IngestT2WMLOutput(object):
         variable_ids = [variables[x]['id'] for x in variables if
                         variables[x].get('is_variable', False) and 'id' in variables[x]]
         return variable_ids
+
+    def convert_t2wml_files(self, t2wml_output_df, item_defs_df):
+        temp_dir = tempfile.mkdtemp()
+        tsv_path = os.path.join(temp_dir, f'kgtk.tsv')
+        exploded_tsv_path = os.path.join(temp_dir, f'kgtk-exploded.tsv')
+        imploded_tsv_path = os.path.join(temp_dir, f'kgtk-imploded.tsv')
+        exploded_tsv_path_with_ids = os.path.join(temp_dir, f'kgtk-exploded-ids.tsv')
+
+        subprocess.run(
+            ['kgtk', 'implode', tsv_path, '-o', imploded_tsv_path, '--allow-lax-qnodes', 'true', '--without',
+             'si_units', 'language_suffix'])
+        if not os.path.isfile(imploded_tsv_path):
+            raise ValueError("Couldn't create imploded TSV file")
+
+        subprocess.run(['kgtk', 'explode', imploded_tsv_path, '-o', exploded_tsv_path, '--allow-lax-qnodes', 'true',
+                            '--overwrite', 'true'])
+        if not os.path.isfile(exploded_tsv_path):
+                raise ValueError("Couldn't create exploded TSV file")
+
+        subprocess.run(['kgtk', 'add-id', '-i', exploded_tsv_path, '-o', exploded_tsv_path_with_ids,
+                            '--overwrite-id', 'true', '--id-style', 'node1-label-node2-num'])
+        print(tsv_path)
+        print(imploded_tsv_path)
+        print(exploded_tsv_path)
+        print(exploded_tsv_path_with_ids)
+
+        return None
