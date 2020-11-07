9af954181b76734099160c98a78f47a2113343c9
Itay Zandbank <itay@researchsoftware.co.il>
2020-11-07 22:13:57 +0200

    dump-datasets Fix minor issues in other scripts

---
+++ script/dump-datasets.py
@@ -0,0 +1,75 @@
+
+import argparse
+import os
+import sys
+
+import requests
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output', type=str, help='Output directory for dump')
+    parser.add_argument('--datamart-api', type=str, default='http://localhost:12543', help="Datamart API URL")
+
+    return parser.parse_args()
+
+datamart_url = None
+def store_datamart_url(url):
+    global datamart_url
+    datamart_url = url
+    if datamart_url[:7] != 'http://' and datamart_url[:8] != 'https://':
+        datamart_url = 'http://' + datamart_url
+    if datamart_url[-1] == '/':
+        datamart_url = datamart_url[:-1]
+
+def datamart_request(url, delete=False, json=True):
+    global datamart_url
+    url = datamart_url + url
+
+    if not delete:
+        response = requests.get(url)
+    else:
+        response = requests.delete(url)
+    if response.status_code > 299 or response.status_code < 200:
+        raise ValueError("Bad response for request to " + url)
+
+    if not json:
+        return response.text
+    return response.json()
+
+def get_datamart_datasets():
+    response = datamart_request('/metadata/datasets')
+    datasets = [d['dataset_id'] for d in response]
+    return datasets
+
+def get_dataset_variables(dataset):
+    response = datamart_request(f'/datasets/{dataset}/variables?limit=1000000', json=False)
+    return response
+
+def dump_variables(dataset, dir):
+    print('Generating dump for ', dataset)
+    dump = get_dataset_variables(dataset)
+    dump_path = os.path.join(dir, f'{dataset}.csv')
+    with open(dump_path, 'w', encoding='utf-8') as of:
+        print(dump, file=of)
+
+def run():
+    args = parse_args()
+    store_datamart_url(args.datamart_api)
+
+    try:
+        os.makedirs(args.output, exist_ok=True)
+    except:
+        print("Can't create output directory ", args.output, file=sys.stderr)
+        return
+
+    datasets = get_datamart_datasets()
+    print(f'Dumping data from {len(datasets)} datasets')
+
+    for dataset in datasets:
+        dump_variables(dataset, args.output)
+
+    print('Done')
+
+if __name__ == '__main__':
+    run()
