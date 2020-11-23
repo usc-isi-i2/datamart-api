e5e3d07780a4b4594f309aac9dbf28c2b464a065
Otamio <ca.john.wang@gmail.com>
2020-11-22 21:51:18 -0800

    modify support for uploading annotated sheets

--- data-refresh/utils/upload.py
+++ data-refresh/utils/upload.py
@@ -6,6 +6,16 @@ from requests import post, put
 from requests.models import Response
 from typing import Dict, Optional
 
+'''
+This module contains the following functions:
+    submit_files() -> submit some files to Datamart with given parameters
+    submit_tsv() -> submit exploded kgtk file to Datamart with given parameters
+    submit_annotated_sheet() -> submit annotated spreadsheet (csv or xlsx) file to Datamart,
+                                  can add optional YAML file and save_tsv path,
+                                  can save t2wml output or exploded kgtk files
+
+'''
+
 sheet_id = 0
 
 def submit_files(url:str, files: Dict, params: Dict) -> Response:
@@ -50,10 +60,12 @@ def submit_tsv(datamart_url: str, file_path: str, put_data: Optional[bool] = Tru
     file_name = os.path.basename(file_path)
     dataset_id = find_dataset_id(file_path)
 
+    # Supply arguments
     url = f'{datamart_url}/datasets/{dataset_id}/tsv'
     files = { 'file': (file_name, open(file_path, mode='rb'), 'application/octet-stream') }
     params = { 'put_data': put_data }
 
+    # Send to Datamart
     response = submit_files(url, files, params)
 
     if response.status_code in [200, 201, 204]:
@@ -63,6 +75,69 @@ def submit_tsv(datamart_url: str, file_path: str, put_data: Optional[bool] = Tru
 
     print(json.dumps(response.json(), indent=2))
     return False
+
+def submit_annotated_sheet(datamart_url: str, annotated_path: str, yaml_path: Optional[str]=None,
+                            put_data: Optional[bool]=False, verbose: Optional[bool] = False,
+                            save_tsv: Optional[str]=None, save_files: Optional[str]=None) -> bool:
+    ''' Submit an annotated sheet
+        Args:
+            datamart_url: Datamart base address
+            annotated_path: The annotated file path
+            yaml_path: The optional yaml file for parsing the dataset
+            put_data: Whether to PUT or POST the data to Datamart
+            verbose: Whether to show variable metadata upon submission success
+            save_tsv: If supplied, the path of the tar.gz file it will be saved
+            save_files: If supplied, the path of the tar.gz file to save T2WML configuration files
+        Returns:
+            A boolean values indicates whether the sheet is uploaded successfully
+    '''
+    def find_dataset_id(file_path: str):
+        if file_path.endswith('.xlsx'):
+            df = pd.read_excel(file_path, header=None, dtype=object).fillna('')
+        else:
+            df = pd.read_csv(file_path, header=None, encoding='latin1', dtype=object).fillna('')
+        return df.iat[0,1]
+
+    if not (annotated_path.endswith('.xlsx') or annotated_path.endswith('.csv')):
+        print(f'Error: Unknown file type - {annotated_path}')
+        return False
+
+    file_name = os.path.basename(annotated_path)
+    dataset_id = find_dataset_id(annotated_path)
+
+    # Supply arguments
+    url = f'{datamart_url}/datasets/{dataset_id}/annotated'
+
+    files = { 'file': (file_name, open(annotated_path, mode='rb'), 'application/octet-stream') }
+    if yaml_path:
+        files['t2wml_yaml'] = os.path.basename(yaml_path), open(yaml_path, mode='rb'), 'application/octet-stream'
+
+    params = { 'put_data': put_data, 'create_if_not_exist': True,
+                'tsv': bool(save_tsv), 'files_only': bool(save_files) }
+
+    # Send to Datamart
+    response = submit_files(url, files, params)
+
+    if not response.status_code in [200, 201, 204]:
+        print(json.dumps(response.json(), indent=2))
+        return False
+
+    # Generate output
+    if save_tsv:
+        if not save_tsv.endswith('.tar.gz'):
+            save_tsv += '-d.tar.gz'
+        with open(save_tsv, 'wb') as fd:
+            fd.write(response.content)
+
+    if save_files:
+        if not save_files.endswith('.tar.gz'):
+            save_files += '-d.tar.gz'
+        with open(save_files, 'wb') as fd:
+            fd.write(response.content)
+
+    if verbose:
+        print(json.dumps(response.json(), indent=2))
+    return True
 
 def upload_data_annotated(url: str, file_path: str, yamlfile_path: str=None,
                             fBuffer: io.StringIO=None, put_data: bool=False) -> bool:
@@ -123,31 +198,6 @@ def submit_sheet(datamart_api_url: str, annotated_sheet: DataFrame,
     if tsv:
         url += '&tsv=true'
     return upload_data_annotated(url, '', None, buffer, put_data)
-
-def submit_annotated_sheet(datamart_api_url: str, annotated_sheet: str, yamlfile_path: str=None,
-                            put_data: bool=False, tsv: bool=False) -> bool:
-    ''' Submit an annotated sheet
-        Args:
-            datamart_api_url: Datamart url
-            annotated_sheet: The annotated sheet path
-        Returns:
-            A boolean values indicates whether the sheet is uploaded successfully
-    '''
-
-    if annotated_sheet.endswith('.xlsx'):
-        df = pd.read_excel(annotated_sheet, header=None, dtype=object).fillna('')
-    elif annotated_sheet.endswith('.csv'):
-        df = pd.read_csv(annotated_sheet, header=None, encoding='latin1', dtype=object).fillna('')
-    else:
-        print(f'Unknown file type: {annotated_sheet}')
-        return False
-
-    dataset_id = df.iat[0,1]
-    url = f'{datamart_api_url}/datasets/{dataset_id}/annotated?create_if_not_exist=true'
-    if tsv:
-        url += '&tsv=true'
-
-    return upload_data_annotated(url, annotated_sheet, yamlfile_path, None, put_data)
 
 def submit_sheet_bulk(datamart_api_url: str, template_path: str, dataset_path: str,
                         flag_combine_sheets: bool=False) -> None:
