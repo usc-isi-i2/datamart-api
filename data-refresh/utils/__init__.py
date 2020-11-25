4f87242754422ab9245ea961e4852aaffa4b3cce
Otamio <ca.john.wang@gmail.com>
2020-11-25 00:53:12 -0800

    add support for getting metadata/data with authentication

--- data-refresh/utils/__init__.py
+++ data-refresh/utils/__init__.py
@@ -3,4 +3,5 @@ from utils.template import *
 from utils.utility import *
 from utils.upload import submit_sheet_bulk, submit_files, submit_tsv
 from utils.erase import *
+from utils.get_ import *
 from utils.test import visualize
