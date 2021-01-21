1ae62ed44807ac5f31412bb4062f935e3596f7f8
Ke-Thia Yao <kyao@isi.edu>
2021-01-20 17:43:23 -0800

    merge conflict

--- docker/docker_config.py
+++ docker/docker_config.py
@@ -1,10 +1,13 @@
 # A configuration file for the Docker image
+
+import os
+
 POSTGRES = dict(
-    database = 'wikidata',
-    host = 'db',
-    port = 5432,
-    user = 'postgres',
-    password = 'postgres',
+    database = os.environ.get('POSTGRES_DB', 'wikidata'),
+    host = os.environ.get('POSTGRES_HOST', 'db'),
+    port = int(os.environ.get('POSTGRES_PORT', 5432)),
+    user = os.environ.get('POSTGRES_USER', 'postgres'),
+    password = os.environ.get('POSTGRES_PASSWORD', 'postgres')
 )
 
 METADATA_DIR = '/src/metadata'
---
+++ docker/initialize-datamart.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# export POSTGRES_HOST=localhost
+# export POSTGRES_PORT=6000
+# export POSTGRES_PASSWORD=empty
+# export POSTGRES_USER=auser
+# export POSTGRES_DB=wikidata2
+
+echo POSTGRES_HOST = $POSTGRES_HOST
+echo POSTGRES_PORT = $POSTGRES_PORT
+echo POSTGRES_PASSWORD = $POSTGRES_PASSWORD
+echo POSTGRES_USER = $POSTGRES_USER
+echo POSTGRES_DB = $POSTGRES_DB
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+until psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -c '\q' $POSTGRES_DB; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+done
+
+# If edges table does not exist, then create the datamart database
+>&2 echo psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -tAc "select to_regclass('public.edges');" $POSTGRES_DB
+result=$(psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -tAc "select to_regclass('public.edges');" $POSTGRES_DB)
+>&2 echo "psql result: $result"
+if [ "$result" != "edges" ]; then
+    >&2 echo "Creating database $POSTGRES_DB"
+    >&2 echo $(ls docker/dev-env/data/postgres/causx.sql)
+    >&2 echo "docker/dev-env/data/postgres/causx.sql | psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_DB"
+    cat docker/dev-env/data/postgres/causx.sql | psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_DB
+fi
+
+gunicorn -b 0.0.0.0:80 --timeout 3600 wsgi:app
