d6c5888594cd52edeca287e2c3e51860b0ac8255
ChanaChelem <chana@chelem.co.il>
2021-04-27 09:52:17 +0300

    datamart --db-host localhost --db-port 5433 --db-name wikidata --db-user postgres --db-password postgres

--- datamart.py
+++ datamart.py
@@ -1,11 +1,7 @@
 # This file runs datamart.
 import os
 import argparse
-from app import app
 
-# TODO: Get the postgres information from the commandline (using argparse)
-# and update the config
-#
 # datamart --db-host localhost --db-port 5433 --db-name wikidata --db-user postgres --db-password postgres
 
 # To test, use Postman and issue a GET to http://localhost:12543/metadata/datasets
@@ -16,33 +12,22 @@ from app import app
 # Go to the jun_devel branch
 # pip install -e ../table-linker
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--db-host", default="localhost", help="DB host")
-# parser.add_argument("--db-port", default="5433", help="DB port")
-# parser.add_argument("--db-name", default="wikidata", help="DB name")
-# parser.add_argument("--db-user", default="postgres", help="DB user")
-# parser.add_argument("--db-password", default="postgres", help="DB password")
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--db-host", default="localhost", help="DB host")
+parser.add_argument("--db-port", default="5433", help="DB port")
+parser.add_argument("--db-name", default="wikidata", help="DB name")
+parser.add_argument("--db-user", default="postgres", help="DB user")
+parser.add_argument("--db-password", default="postgres", help="DB password")
+args = parser.parse_args()
 
+os.environ["DB_HOST"] = args.db_host
+os.environ["DB_PORT"] = args.db_port
+os.environ["DB_NAME"] = args.db_name
+os.environ["DB_USER"] = args.db_user
+os.environ["DB_PASSWORD"] = args.db_password
 
-# app.config.update(
-#     POSTGRES = dict(
-#         database = args.db_name,
-#         host = args.db_host,
-#         port = args.db_port,
-#         user = args.db_user,
-#         password = args.db_password
-#     )
-# )
-
-# os.environ["DB_HOST"] = args.db_host
-# os.environ["DB_PORT"] = args.db_port
-# os.environ["DB_NAME"] = args.db_name
-# os.environ["DB_USER"] = args.db_user
-# os.environ["DB_PASSWORD"] = args.db_password
-# print("put postgres")
+from app import app
 
 if __name__ == '__main__':
-    # print("start app")
-    # print(app.config.get("POSTGRES"))
+    print(app.config.get("POSTGRES"))
     app.run(port=12543)
