6e5f01b62d5a4f7c7bd54648e4b3c25c4b716b88
Ke-Thia Yao <kyao@isi.edu>
2021-05-16 16:25:34 -0700

    update patch, dockerfile

--- dev-env/data/postgres/patches.sql
+++ dev-env/data/postgres/patches.sql
@@ -12,3 +12,8 @@ DELETE FROM edges WHERE node2='Q200464' or node1='Q200464';
 delete from edges where id='"Q14818820-label-""Fragile States Index""-0000"';
 delete from edges where id='Q8035788-label-"economic fitness dataset"-0000';
 delete from edges where id='Q8035788-label-"World Development Indicators"-0000';
+
+-- Add qndoes
+
+insert into edges values   ('Q7184-label-0', 'Q7184', 'label', 'NATO''@en', 'language_qualified_string');
+insert into strings values ('Q7184-label-0', 'NATO', 'en');
