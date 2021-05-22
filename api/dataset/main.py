eec34d447395d36694b868308bff1d6c926ddd3b
Ke-Thia Yao <kyao@isi.edu>
2021-05-22 16:09:46 -0700

    add back post dataset w/ unit test

--- api/dataset/main.py
+++ api/dataset/main.py
@@ -4,13 +4,12 @@ from .post import DatasetData
 
 class DatasetResource(Resource):
     def post(self, dataset):
-        raise NotImplementedError("kgtk support has been thorougly disabled")
         post_data = DatasetData()
         return post_data.process(dataset)
 
     def put(self, dataset):
         return {'Error': 'The PUT method is not allowed.'}
-        
+
     def get(self, dataset):
         return {'Error': 'The GET method is currently not implemented.'
                          f" Use the endpoint 'datasets/{dataset}/variables' instead"}
